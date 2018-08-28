037232f81017769817506f5dd6ad90caf6e9e113
Nurbol Alpysbayev <anurbol@gmail.com>
2018-08-28 11:21:37 +0600

    .

--- src/VAR.state.ts
+++ src/VAR.state.ts
@@ -1,10 +1,22 @@
 // tslint:disable:max-file-line-count
-import { Subject } from 'rxjs'
+import { Subject, Subscription } from 'rxjs'
+import { filter, takeUntil } from 'rxjs/operators'
 import { modes, TMode } from './ENM.modes'
 import { CONSTANTS } from './VAR.constants'
 import { settings } from './VAR.settings'
 
-export type TResult = any[] // todo 
+export type TResult = any[] // todo
+
+type TChange = {
+    previous: number,
+    new: number,
+}
+
+type TChangesSubject = Subject<TChange> & {
+    subscriptions: {
+        [key: string]: Subscription,
+    },
+}
 
 export const state = {
     input: {
@@ -21,20 +33,34 @@ export const state = {
     },
     _pos: 0,
     set pos(val) {
+        const previous = this._pos
+
         this._pos = val
-        this.pos$.next(this._pos)
+
+        const posChange: TChange = {
+            previous,
+            new: this._pos,
+        }
+
+        this.pos$.next(posChange)
     },
     get pos() {
         return this._pos
     },
-    get pos$() {
-        return new Subject()
+    get pos$(): TChangesSubject {
+        // tslint:disable-next-line:prefer-const
+        const subject = new Subject<TChange>()
+        const changesSubject: TChangesSubject = subject as TChangesSubject
+
+        changesSubject.subscriptions = {}
+
+        return changesSubject
     },
     char: {
         val: null as string | null,
         get isWhitespace() {
             if (this.val === null) {
-                state.error(`state.char.val cannot be null,
+                throw state.error(`state.char.val cannot be null,
                 the program definitely has some error`)
             }
 
@@ -94,8 +120,8 @@ export const state = {
             addCharToName() {
                 this.name += state.char.val
             },
-            addCharToValue() {
-                this.value += state.char.val
+            addCharsToValue(chars: string) {
+                this.value += chars
             },
             finish() {
                 this.name = ''
@@ -123,7 +149,7 @@ export const state = {
         this.mode.val = modes.NON_SECTIONED_CONTENT_IN_PROGRESS
 
         if (this.char.val === null) {
-            state.error('state.char cannot be null, the program definitely has some error')
+            throw state.error('state.char cannot be null, the program definitely has some error')
         }
 
         this.nonSectionedContentBuffer.add(this.char.val)
@@ -136,7 +162,7 @@ export const state = {
 
         if (this.pos > 0) {
             if (offset <= 0) {
-                state.error(`The passed offset can equal 0
+                throw state.error(`The passed offset can equal 0
                 only at the very first iteration of state.next().
                 \Each next iteration should have greater than 0 offset`)
             }
@@ -217,12 +243,12 @@ export const state = {
                             this.mode.set(modes.FILESECTION_BODY_IN_PROGRESS)
                             return this.next(1)
                         } else {
-                            state.error(`Filesection must have at least
+                            throw state.error(`Filesection must have at least
                             one of these attributes: "vendor", "name"`)
                         }
                     }
 
-                    state.error(`Unexpected token "${this.char.val}",
+                    throw state.error(`Unexpected token "${this.char.val}",
                     expected attribute name`)
                 // todo at file, line, position
 
@@ -244,7 +270,7 @@ export const state = {
                         return this.next(1)
                     }
 
-                    state.error(`Unexpected token "${this.char.val}",
+                    throw state.error(`Unexpected token "${this.char.val}",
                     expected valid character for attribute name`)
                 // todo at file, line, position
                 case modes.EXPECT_EQUAL_SIGN_BETWEEN_ATTRIBUTE_AND_VALUE:
@@ -252,7 +278,7 @@ export const state = {
                         this.mode.set(modes.EXPECT_ATTRIBUTE_VALUE)
                         return this.next(1)
                     }
-                    state.error(`Unexpected token "${this.char.val}", expected equal sign`)
+                    throw state.error(`Unexpected token "${this.char.val}", expected equal sign`)
                 case modes.EXPECT_ATTRIBUTE_VALUE:
                     if (this.char.isWhitespace) {
                         return this.next(1)
@@ -271,19 +297,31 @@ export const state = {
                         return this.next(1)
                     }
 
-                    state.error(`Unexpected token "${this.char.val}",
+                    throw state.error(`Unexpected token "${this.char.val}",
                     expected attribute value`)
                 case modes.QUOTTED_ATTRIBUTE_VALUE_IN_PROGRESS:
+
+                    this.changes$.pipe(
+                        (takeUntil(this.mode.val.changes$.pipe(
+                            filter((change: TChange) => change.new !== modes.QUOTTED_ATTRIBUTE_VALUE_IN_PROGRESS),
+                        ))),
+                    ).subscribeOnce((posChange) => {
+                        // every time state.pos changes while in this QUOTTED_ATTRIBUTE_VALUE_IN_PROGRESS mode,
+                        // add processed chars to the current attribute's value
+                        // (attribute has name and value, we add to the latter)
+
+                        // also consider cases when delta between prev and curr is more than one,
+                        // in this case all processed characters should be added to attributes value
+                        const processedChars = this.input.val.substring(posChange.previous, posChange.new)
+
+                        this.currentFilesection.currentAttribute.addCharsToValue(processedChars)
+                    })
+
+
+
                     if (this.char.val === this.quotedContentInProgress.quoteType) {
 
-                        this.pos$.subscribe((newPos) => {
-                            change.previous
-                            change.current
 
-                            // todo consider cases when delta between prev and curr is more than one, in this case all processed characters should be added to attributes value
-
-                            this.currentFilesection.currentAttribute.addCharToValue()
-                        })
 
                         // skip the case when the quote is escaped
                         if (this.input.val.charAt(this.pos - 1) === '\\') {
@@ -298,13 +336,13 @@ export const state = {
                     }
 
                     if (this.pos >= this.input.val.length) {
-                        state.error(`An unclosed quote encountered.
+                        throw state.error(`An unclosed quote encountered.
                         The opening quote is located at `)
                         // todo file? line and position
                     }
 
                     if (this.char.val === '\n') {
-                        state.error(`The attribute value must not contain a new line`)
+                        throw state.error(`The attribute value must not contain a new line`)
                     }
 
                     // any character other than unescaped quote and new line is ok in a quotted string
@@ -319,13 +357,13 @@ export const state = {
                     throw??
                 // todo 
                 default:
-                    state.error('unexpected mode: ' + state.mode)
+                    throw state.error('unexpected mode: ' + state.mode)
             }
         })()
 
-        state.error('program somehow has reached this point, though it MUST NOT')
+        throw state.error('program somehow has reached this point, though it MUST NOT')
     },
     error(msg: string) {
-        throw new Error(`[filesections-parser] ${msg}`)
+        return new Error(`[filesections-parser] ${msg}`)
     },
 }
